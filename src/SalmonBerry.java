f452b70f066b7fe1a2d3f7ccb569db8a3e3355fc
italofini <italo.fini@techhigh.us>
2015-08-25 23:52:17 -0400

    do while Loops

---
+++ src/SalmonBerry.java
@@ -0,0 +1,15 @@
+public class SalmonBerry {
+	public static void main(String[] args){
+		int age = 45;
+		
+		if (age > 60)
+			System.out.println("You are a senoir citzen");
+		else if (age>=50)
+		System.out.println("You are in your 50s");
+		else if (age>=40)
+		System.out.println("You are in your 40s");
+		else 
+			System.out.println("You are a young buck");
+	}
+
+}
